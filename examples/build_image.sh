0fc9bffe76b1e2b55e3ff707c8b0db64fde04f7b
llhuii <liulinghui@huawei.com>
2021-03-29 10:38:15 +0800

    Make the name of examples images more meaningful
    Signed-off-by: llhuii <liulinghui@huawei.com>

--- examples/build_image.sh
+++ examples/build_image.sh
@@ -1,8 +1,19 @@
 cd $(cd $(dirname ${BASH_SOURCE[0]}); pwd)
-docker build -f ji-little.Dockerfile -t kubeedge/sedna-example-ji-little:v0.1.0 ..
-docker build -f ji-big.Dockerfile -t kubeedge/sedna-example-ji-big:v0.1.0 ..
 
-docker build -f fl-agg.Dockerfile -t kubeedge/sedna-example-fl-agg:v0.1.0 ..
-docker build -f fl-train.Dockerfile -t kubeedge/sedna-example-fl-train:v0.1.0 ..
+IMAGE_REPO=${IMAGE_REPO:-kubeedge}
+IMAGE_TAG=${IMAGE_TAG:-v0.1.0}
 
-docker build -f il.Dockerfile -t kubeedge/sedna-example-il:v0.1.0 ..
+EXAMPLE_REPO_PREFIX=${IMAGE_REPO}/sedna-example-
+
+dockerfiles=(
+federated-learning-surface-defect-detection-aggregation.Dockerfile
+federated-learning-surface-defect-detection-train.Dockerfile
+incremental-learning-helmet-detection.Dockerfile
+joint-inference-helmet-detection-big.Dockerfile
+joint-inference-helmet-detection-little.Dockerfile
+)
+
+for dockerfile in ${dockerfiles[@]}; do
+  example_name=${dockerfile/.Dockerfile}
+  docker build -f $dockerfile -t ${EXAMPLE_REPO_PREFIX}${example_name}:${IMAGE_TAG} --label sedna=examples ..
+done
