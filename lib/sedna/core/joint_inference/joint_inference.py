9859921b14f7b217d569af4768990d2212dc5949
Vittorio Cozzolino <vittorio.cozzolino@huawei.com>
2021-07-13 21:52:48 +0800

    Edit log messg.

--- lib/sedna/core/joint_inference/joint_inference.py
+++ lib/sedna/core/joint_inference/joint_inference.py
@@ -148,8 +148,8 @@ class JointInference(JobBase):
                 with FTimer(f"{os.uname()[1]}_cloud_inference_and_transmission"):
                     cloud_result = self.cloud.inference(
                         data.tolist(), post_process=post_process, **kwargs)
-                                  
+
                 size = sys.getsizeof(0) * len(flatten_nested_list(cloud_result))
-                self.log.info(f"Received data: {size} bytes")
+                self.log.info(f"Received data: {size} bytes. {cloud_result}")
                 self.lc_reporter.update_for_collaboration_inference()
         return [is_hard_example, res, edge_result, cloud_result]
