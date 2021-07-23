704d9477aa51ce4e1f5b4a26be0b67e99a8c4a29
llhuii <liulinghui@huawei.com>
2021-07-23 11:21:08 +0800

    gm: add dataset controller
    Only handle dataset update from edge.
    
    Signed-off-by: llhuii <liulinghui@huawei.com>

--- pkg/globalmanager/controllers/dataset/dataset.go
+++ pkg/globalmanager/controllers/dataset/dataset.go
@@ -3,42 +3,43 @@ package dataset
 import (
 	"context"
 	"encoding/json"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	sednav1 "github.com/kubeedge/sedna/pkg/apis/sedna/v1alpha1"
 	"github.com/kubeedge/sedna/pkg/globalmanager/config"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/cache"
+	"github.com/kubeedge/sedna/pkg/globalmanager/runtime"
 
 	sednaclientset "github.com/kubeedge/sedna/pkg/client/clientset/versioned/typed/sedna/v1alpha1"
-	sednav1listers "github.com/kubeedge/sedna/pkg/client/listers/sedna/v1alpha1"
-	"github.com/kubeedge/sedna/pkg/globalmanager/runtime"
+)
+
+const (
+	// KindName is the kind name of CR this controller controls
+	KindName = "Dataset"
+
+	// Name is this controller name
+	Name = "Dataset"
 )
 
 // Controller handles all dataset objects including: syncing to edge and update from edge.
 type Controller struct {
 	client sednaclientset.SednaV1alpha1Interface
 
-	storeSynced cache.InformerSynced
-
-	// A store of dataset
-	lister sednav1listers.DatasetLister
-
 	cfg *config.ControllerConfig
 }
 
-// updateDatasetFromEdge syncs update from edge
-func (c *Controller) updateDatasetFromEdge(name, namespace, operation string, content []byte) error {
+// updateFromEdge syncs update from edge
+func (c *Controller) updateFromEdge(name, namespace, operation string, content []byte) error {
 	status := sednav1.DatasetStatus{}
 	err := json.Unmarshal(content, &status)
 	if err != nil {
 		return err
 	}
 
-	return c.updateDatasetStatus(name, namespace, status)
+	return c.updateStatus(name, namespace, status)
 }
 
-// updateDatasetStatus updates the dataset status
-func (c *Controller) updateDatasetStatus(name, namespace string, status sednav1.DatasetStatus) error {
+// updateStatus updates the dataset status
+func (c *Controller) updateStatus(name, namespace string, status sednav1.DatasetStatus) error {
 	client := c.client.Datasets(namespace)
 
 	if status.UpdateTime == nil {
@@ -55,4 +56,20 @@ func (c *Controller) updateDatasetStatus(name, namespace string, status sednav1.
 		_, err = client.UpdateStatus(context.TODO(), dataset, metav1.UpdateOptions{})
 		return err
 	})
+}
+
+func (c *Controller) Run(stopCh <-chan struct{}) {
+	// noop now
+}
+
+// New creates a dataset controller
+func New(cc *runtime.ControllerContext) (runtime.FeatureControllerI, error) {
+	c := &Controller{
+		client: cc.SednaClient.SednaV1alpha1(),
+	}
+
+	// only upstream
+	cc.UpstreamController.Add(KindName, c.updateFromEdge)
+
+	return c, nil
 }
--- pkg/globalmanager/controllers/registry.go
+++ pkg/globalmanager/controllers/registry.go
@@ -1,8 +1,25 @@
+/*
+Copyright 2021 The KubeEdge Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package controllers
 
 import (
 	"fmt"
 
+	"github.com/kubeedge/sedna/pkg/globalmanager/controllers/dataset"
 	fl "github.com/kubeedge/sedna/pkg/globalmanager/controllers/federatedlearning"
 	il "github.com/kubeedge/sedna/pkg/globalmanager/controllers/incrementallearning"
 	ji "github.com/kubeedge/sedna/pkg/globalmanager/controllers/jointinference"
@@ -14,19 +31,12 @@ type FeatureFactory = func(*runtime.ControllerContext) (runtime.FeatureControlle
 
 type Registry map[string]FeatureFactory
 
-func (r Registry) Register(name string, factory FeatureFactory) error {
-	if _, ok := r[name]; ok {
-		return fmt.Errorf("a feature controller named %s already exists", name)
-	}
-	r[name] = factory
-	return nil
-}
-
 func NewRegistry() Registry {
 	return Registry{
-		ji.Name: ji.New,
-		fl.Name: fl.New,
-		il.Name: il.New,
-		ll.Name: ll.New,
+		ji.Name:      ji.New,
+		fl.Name:      fl.New,
+		il.Name:      il.New,
+		ll.Name:      ll.New,
+		dataset.Name: dataset.New,
 	}
 }
