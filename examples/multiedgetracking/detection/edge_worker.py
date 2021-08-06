4d65e6b01f5aab8588169432ccbd563cf17e31e6
Vittorio Cozzolino <vittorio.cozzolino@huawei.com>
2021-08-06 19:29:12 +0800

    Fix container detector code.

--- examples/multiedgetracking/detection/edge_worker.py
+++ examples/multiedgetracking/detection/edge_worker.py
@@ -19,7 +19,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as T
 import numpy as np
-from utils import non_max_suppression, apply_classifier, letterbox
+from utils import non_max_suppression, apply_classifier, letterbox, scale_coords
 
 from sedna.common.config import Context
 from sedna.common.benchmark import FTimer
@@ -73,25 +73,38 @@ def attempt_load(weights, map_location=None, inplace=True):
     ckpt = torch.load(weights, map_location=map_location)  # load
     model.append(ckpt['ema' if ckpt.get('ema') else 'model'].float().fuse().eval())  # FP32 model
 
+    if len(model) == 1:
+        return model[-1]  # return model
+    else:
+        print(f'Ensemble created with {weights}\n')
+        for k in ['names']:
+            setattr(model, k, getattr(model[-1], k))
+        model.stride = model[torch.argmax(torch.tensor([m.stride.max() for m in model])).int()].stride  # max stride
+        
+    return model
+
 class Estimator:
     def __init__(self):      
-        self.webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
-            ('rtsp://', 'rtmp://', 'http://', 'https://'))
+        #self.webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
+        #    ('rtsp://', 'rtmp://', 'http://', 'https://'))
 
         # Initialize
+        LOGGER.info("Starting estimator")
         self.device = self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.classify = True
+        self.classify = False
         self.weights = model_weights
         self.stride, self.names = 64, [f'class{i}' for i in range(1000)]  # assign defaults
         self.img_size = image_size
     
     def load(self, model_url="", mmodel_name=None, **kwargs):
+        LOGGER.info("Loading model")
         self.model = attempt_load(self.weights, map_location=self.device)  # load FP32 model
         self.stride = int(self.model.stride.max())  # model stride
         self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names  # get class names
 
-        self.modelc = load_classifier(name='resnet50', n=2)  # initialize
-        self.modelc.load_state_dict(torch.load('resnet50.pt', map_location=self.device)['model']).to(self.device).eval()
+        #LOGGER.info("Loading classifier")
+        #self.modelc = load_classifier(name='resnet50', n=2)  # initialize
+        #self.modelc.load_state_dict(torch.load(classifier, map_location=self.device)['model']).to(self.device).eval()
 
     def evaluate(self, **kwargs):
         LOGGER.info(f"Evaluating model")
@@ -99,12 +112,15 @@ class Estimator:
 
     def predict(self, im0s, **kwargs):
             # Padded resize
+            print(im0s.shape)
+            LOGGER.info("Manipulating source image")
             img = letterbox(im0s, self.img_size, stride=self.stride)[0]
 
             # Convert
             img = img.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
             img = np.ascontiguousarray(img)
 
+            LOGGER.info("Loading image to device")
             img = torch.from_numpy(img).to(self.device)
             img = img.float()  # uint8 to fp16/32
 
@@ -113,29 +129,45 @@ class Estimator:
                 img = img[None]  # expand for batch dim
 
             # Inference
-            with FTimer(f"inference"):
-                with torch.no_grad():
-                    pred = self.model(img, augment=False, visualize=False)[0]
+            with torch.no_grad():
+                LOGGER.info("Inference")
+                pred = self.model(img, augment=False, visualize=False)[0]
 
             # NMS (this steps should output the tensor)
-            with FTimer(f"nms"):
-                pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
+            LOGGER.info("NMS")
+            pred = non_max_suppression(pred, 0.25, 0.45, None, False, max_det=1000)
 
 
             # Second-stage classifier (optional)
             if self.classify:
-                with FTimer(f"classify"):
-                    pred = apply_classifier(pred, self.modelc, img, im0s)
+                LOGGER.info("classify")
+                pred = apply_classifier(pred, self.modelc, img, im0s)
 
             # # Process predictions
-            # for i, det in enumerate(pred):  # detections per image
+            s = ""
+            for i, det in enumerate(pred):  # detections per image
 
-            #     imc = im0.copy()
-            #     if len(det):
-            #         # Rescale boxes from img_size to im0 size
-            #         det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+                 imc = im0s.copy()
+                 if len(det):
+                   # Rescale boxes from img_size to im0 size
+                   det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0s.shape).round()
+                                    
+                   # Print results
+                   for c in det[:, -1].unique():
+                     n = (det[:, -1] == c).sum()  # detections per class
+                     s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                 
+                 # Write results
+                   #for *xyxy, conf, cls in reversed(det):
+                     #c = int(cls)  # integer class
+                     #label = f'{self.names[c]} {conf:.2f}'
+                     #plot_one_box(xyxy, im0s, label=label, color=colors(c, True))
+                     #save_one_box(xyxy, imc, file='test.jpg', BGR=True)
+                 
+                   # Write results
+                   bbs_list = []
+                   for conf in reversed(det):
+                       bbs_list.append([imc, conf])
 
-            #         # Write results
-            #         bbs_list = []
-            #         for _, conf, _ in reversed(det):
-            #             bbs_list.append([imc, conf])
+            LOGGER.debug(bbs_list[0])
+            LOGGER.info(s)
