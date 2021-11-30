a3e6e4018951d40487400392e07e16e343d8c6fd
t00413490 <tantaiyunlong@huawei.com>
2021-11-30 15:11:42 +0800

    update

--- examples/build_agg.sh
+++ examples/build_agg.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker build -f federated-learning-mistnet-yolo-aggregator.Dockerfile -t kubeedge/sedna-example-federated-learning-mistnet-yolo-aggregator:v0.4.0 --label sedna=examples ..
-#docker build -f federated-learning-mistnet-yolo-client.Dockerfile -t kubeedge/sedna-example-federated-learning-mistnet-yolo-client:v0.4.0 --label sedna=examples ..
+docker build -f mistnet-yolo-aggregator.Dockerfile -t mistnet-yolo-server:v0.4.0 --label sedna=examples --no-cache ..
