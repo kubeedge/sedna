8b667b649b93a715f7a03cf805b3215d2694cab8
t00413490 <tantaiyunlong@huawei.com>
2021-12-02 11:40:27 +0800

    update

--- examples/build_train.sh
+++ examples/build_train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -f mistnet-yolo-client.Dockerfile -t mistnet-yolo-client:v0.4.0 --label sedna=examples --no-cache ..
+docker build -f mistnet-yolo-client.Dockerfile -t decshub.org/mistnet-yolo-client:v0.4.0 --label sedna=examples --no-cache ..
