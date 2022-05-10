eb43b3b3fa98dbd278158e48f3265406417f61d7
c59900052 <cuijinkai@huawei.com>
2022-05-10 10:26:15 +0800

    TEST

--- examples/federated_learning/yolov5_coco128_mistnet/interface.py
+++ examples/federated_learning/yolov5_coco128_mistnet/interface.py
@@ -171,8 +171,8 @@ class Estimator_server:
             "target_accuracy": 0.99,
             "epochs": int(Context.get_parameters("EPOCHS", 500)),
             "batch_size": int(Context.get_parameters("BATCH_SIZE", 16)),
-            "max_epoch": 200,
-            "per_batch_size": 8,
+            "max_epoch": 2,
+            "per_batch_size": 1,
             "optimizer": "SGD",
             "linear_lr": False,
             # The machine learning model
