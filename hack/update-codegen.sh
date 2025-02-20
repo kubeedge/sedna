e7de7896ddedb26f8649571cf8a7fcb5526fb33a
ming.tang <ming.tang@daocloud.io>
2025-02-20 11:36:40 +0800

    fix code-gen can not surport multi version
    Signed-off-by: ming.tang <ming.tang@daocloud.io>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -22,7 +22,16 @@ SEDNA_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 
 source ${SEDNA_ROOT}/hack/lib/init.sh
 
-${SEDNA_ROOT}/hack/generate-groups.sh "deepcopy,client,informer,lister" \
-  ${SEDNA_GO_PACKAGE}/pkg/client ${SEDNA_GO_PACKAGE}/pkg/apis \
-  "sedna:v1alpha1" \
-  --go-header-file ${SEDNA_ROOT}/hack/boilerplate/boilerplate.generatego.txt
+API_VERSIONS=$(ls -d "${SEDNA_ROOT}/pkg/apis/sedna"/*/ 2>/dev/null | xargs -n 1 basename | sort | paste -sd, -)
+
+if [[ -z "${API_VERSIONS}" ]]; then
+    echo "Error: No API versions found in ${SEDNA_GO_PACKAGE}/pkg/apis/sedna"
+    exit 1
+fi
+
+GROUP_VERSIONS="sedna:${API_VERSIONS}"
+
+"${SEDNA_ROOT}/hack/generate-groups.sh" "deepcopy,client,informer,lister" \
+  "${SEDNA_GO_PACKAGE}/pkg/client" "${SEDNA_GO_PACKAGE}/pkg/apis" \
+  "${GROUP_VERSIONS}" \
+  --go-header-file "${SEDNA_ROOT}/hack/boilerplate/boilerplate.generatego.txt"
